c3cd8c8d1cb861da6ee561652b7a2412517db5f7
penglezos <panagiotisegl@gmail.com>
2021-08-21 16:49:29 +0300

    raphaeL: Set TARGET_KERNEL_SOURCE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,8 +58,8 @@ endif
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150
-TARGET_KERNEL_CONFIG := raphael_user_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
+TARGET_KERNEL_CONFIG := raphael_defconfig
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
